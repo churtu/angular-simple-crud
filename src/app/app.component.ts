ba167a23577e2ecfc89773382f7ca2e7e5eecfa1
Christian Leandro Urtubia Rodriguez <ch.urtubia@alumnos.duoc.cl>
2020-01-20 15:06:35 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  selectedEmployee:Employee = new Employee();
+  employees : Employee[] = [
+    {
+      id: 1, name: "Christian", country: "Chile"
+    },
+    {
+      id: 2, name: "Cami", country: "Chile"
+    },
+    {
+      id: 3, name: "Juan", country: "Brazil"
+    }
+  ];
+
+  addOrEdit():void{
+    if(this.selectedEmployee.id == 0){
+      this.selectedEmployee.id = this.employees.length+1;
+      this.employees.push(this.selectedEmployee);
+    }else{
+      this.selectedEmployee = new Employee();
+    }
+    
+  }
+
+  openForEdit(employee:Employee){
+    this.selectedEmployee = employee;
+  }
+
+  delete(){
+    if(confirm('Are you sure?')){
+      this.employees = this.employees.filter(
+        employee => employee != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
